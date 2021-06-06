e20133cac5994dfb54f92542634682d9c4e443a8
Chandresh Bhatt <33648618+BhattChandresh@users.noreply.github.com>
2021-06-06 17:59:32 +0530

    python practice

---
+++ Day4/Question15.py
@@ -0,0 +1,19 @@
+# Question:
+# Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+
+print("-- Method-1 --")
+print("Enter a number : ")
+number = input()
+my_equation = "a+aa+aaa+aaaa"
+math_equation = my_equation.replace("a", number)
+my_list = math_equation.split("+")
+result = 0
+for item in my_list:
+    result = result + int(item)
+print(result)
+
+print("-- Method-2 --")
+print("Enter a number : ")
+number1 = input()
+my_sum = int(number1) + int(2*number1) + int(3*number1) + int(4*number1)
+print(my_sum)
